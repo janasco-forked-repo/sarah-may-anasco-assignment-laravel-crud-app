f0ca98581f6c7371c9b05418922a2b77e9e87422
Jaymar Anasco <jaymaranasco@gmail.com>
2023-01-30 14:17:55 +0800

    Update index.blade.php

--- resources/views/iphones/index.blade.php
+++ resources/views/iphones/index.blade.php
@@ -8,10 +8,16 @@
             <div class="pull-left">
                 <h2>Laravel 9 CRUD Assignment - Sarah May Anasco</h2>
             </div>
+            <br>
             <div class="pull-right">
-                <a class="btn btn-success" href="{{ route('iphones.create') }}"> Create New iPhone</a>
+                <a class="btn btn-success" href="{{ route('iphones.create') }}"> <strong>C</strong>reate New iPhone</a>
             </div>
+            <br>
         </div>
+        <div><strong>C</strong> - <p>Create-</p></div>
+        <div><strong>R</strong> - <p>Read-</p></div>
+        <div><strong>U</strong> - <p>Update-</p></div>
+        <div><strong>D</strong>  <p>Delete</p></div>
     </div>
     <hr>
     <br>
@@ -22,7 +28,7 @@
         </div>
     @endif
    <div class="row">
-	    <table id="iPhoneTables" class="table table-bordered">
+	    <table id="iPhoneTables" class="table table-bordered table-striped">
 	        <thead>
 		        <tr>
 		            <th>No</th>
@@ -33,21 +39,21 @@
 		    </thead>
 	        <tbody>
 		        @foreach ($iphones as $iphone)
-		        <tr>
+		        <tr >
 		            <td>{{ ++$i }}</td>
 		            <td>{{ $iphone->name }}</td>
 		            <td>{{ $iphone->detail }}</td>
 		            <td>
 		                <form action="{{ route('iphones.destroy',$iphone->id) }}" method="POST">
 		   
-		                    <a class="btn btn-info" href="{{ route('iphones.show',$iphone->id) }}">View</a>
+		                    <a class="btn btn-info" href="{{ route('iphones.show',$iphone->id) }}"><strong>R</strong>ead</a>
 		    
-		                    <a class="btn btn-primary" href="{{ route('iphones.edit',$iphone->id) }}">Edit</a>
+		                    <a class="btn btn-primary" href="{{ route('iphones.edit',$iphone->id) }}"><strong>U</strong>pdate</a>
 		   
 		                    @csrf
 		                    @method('DELETE')
 		      
-		                    <button type="submit" class="btn btn-danger">Delete</button>
+		                    <button type="submit" class="btn btn-danger"><strong>D</strong>elete</button>
 		                </form>
 		            </td>
 		        </tr>
